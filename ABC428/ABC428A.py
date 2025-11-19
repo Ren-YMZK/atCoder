bd9383f4c8177ed72455a9b513b8a83361b4b825
Ren-YMZK <r26763041@gmail.com>
2025-11-19 20:40:34 +0900

    A クリア！！！

--- ABC428/ABC428A.py
+++ ABC428/ABC428A.py
@@ -1,1 +1,25 @@
 S, A, B, X = map(int, input().split())
+
+# 1周期の長さ
+one_cycle_time = A + B
+
+# 1周期の回数
+how_many_cycle = X // one_cycle_time
+
+# 余った時間
+amari_time = X % one_cycle_time
+
+# 1周期で走った時間
+cycle_run_time = how_many_cycle * A
+
+#あまり時間で走った時間
+if amari_time < A:
+  amari_run_time = amari_time
+else:
+  amari_run_time = A
+
+run_time_sum = how_many_cycle * A + amari_run_time
+
+run_meters = run_time_sum * S
+
+print(run_meters)
